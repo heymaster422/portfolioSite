2a3f883156b69bd5ef88780541ab7e613915c446
Andrew Wong <andrew.w14g@gmail.com>
2021-12-18 10:16:43 -0800

    Finished Navbar, projects, and skills

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const educations = [
+  {
+    school: "Pasadena Community College",
+    degree: "AS-T in Physics , Mathetmatics, Engineering & Technology",   
+  },
+  {
+    school: "Cal State Northridge",
+    degree: "B.S. Software Engineering & Technology",   
+    gpa: "[Expected Grad Spring 2022]"
+  },
+];
+
+export const experiences = [
+  {
+    quote:
+      "Duties include managing online sales channels by collecting data/SEO, custom website html editing, obtaining licenses for our company (FDA, ISO 9001), backing up essential files, designing and printing labels for facial product, sourcing inventory/packaging internationally.",
+    image: "./SMB.webp",
+    companyName: "San Marino Beauty",
+    date: "March 2019 - June. 2021",
+    position: "Administrative Assistant",
+  },
+  {
+    quote:
+      "Duties as shift leader included hiring, ordering inventory, organizing inventory, training new employees, and sales.",
+    image: "./br logo.png",
+    companyName: "Baskin Robbins",
+    date: "May 2016 - May 2018",
+    position: "Shift Leader",
+  },
+];
+
+export const skills = ["Java","C","C++","Python","JavaScript","HTML","CSS","Docker","SQL Injection & Buffer Overflow","Jira","Git","Unity Game Engine"];
